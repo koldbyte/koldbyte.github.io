1f08b604f02724f40b6fb6b787ab4ba06e5fce88
Bhaskar Divya <bhaskar.divya007@gmail.com>
2021-10-11 20:45:19 +0530

    Add a nuxtjs based simple site with auto deploy

---
+++ components/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <a class="flex justify-center pt-8 sm:pt-0" target="_blank">
+        <img class="avatar" src="avatar.jpg">
+      </a>
+      <h2 class="hero-text">
+          BHASKAR DIVYA
+      </h2>
+      <h2 class="hero-text">
+          Principal Engineer @ Reflektion (Data Platform Team)
+      </h2>
+      <h2 class="hero-text">
+          Generalist | Developer | Data Engineer | Maker
+      </h2>
+
+      <div class="flex justify-center pt-4 space-x-4">
+        <a href="https://github.com/koldbyte" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+            width="60" height="60"
+            viewBox="0 0 30 30"
+            style=" fill:#000000;">    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path></svg>
+        </a>
+        
+        <a href="https://linkedin.com/in/bhaskardivya" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+            width="60" height="60"
+            viewBox="0 0 30 30"
+            style=" fill:#000000;">    
+              <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path></svg>
+        </a>
+
+        <a href="Resume.pdf" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="60" height="60"
+          viewBox="0 0 50 50"
+          style=" fill:#000000;"><path d="M 7 2 L 7 48 L 43 48 L 43 14.59375 L 42.71875 14.28125 L 30.71875 2.28125 L 30.40625 2 Z M 9 4 L 29 4 L 29 16 L 41 16 L 41 46 L 9 46 Z M 31 5.4375 L 39.5625 14 L 31 14 Z M 15 22 L 15 24 L 35 24 L 35 22 Z M 15 28 L 15 30 L 31 30 L 31 28 Z M 15 34 L 15 36 L 35 36 L 35 34 Z"></path></svg>
+        </a>
+      </div>
+
+      <div class="mt-4 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <p class="mt-2 text-gray-800">
+           A Computer Science major from Birla Institute of Technology, Mesra with 6.5+ years of experience in problem-solving in a fast-paced high-growth environment.
+        </p>
+        <p class="mt-2 text-gray-800">
+          A Developer by skill with a decent Product building experience.
+        </p>
+        <p class="mt-2 text-gray-800">
+          With me I bring big data & full stack development experience, quick learning ability and polyglot programming skills (Golang, Python, Java, C++).
+        </p>
+        <p class="mt-2 text-gray-800">
+          With expertise on the big data technologies like Flink, Kafka, Airflow, DynamoDB, Snowflake, Hadoop, Spark and others gives me good understanding of nuances of distributed system, scalability and availability.
+        </p>
+        <p class="mt-4 text-gray-800">
+          Apart from the professional work experience, I have been building mini-products and side-projects since my middle school to learn more and improve my coding skills.
+          The tech stack varied from App Development(Flutter/Dart), C++/Qt, Web Dev(LAMP, HTML, CSS, JS, VueJS, jQuery, Bootstrap) and J2EE.
+        </p>
+        <p class="mt-2 text-gray-800 font-medium">
+          A Generalist by heart. A little introvert by nature.
+        </p>
+        <p class="mt-4 text-gray-800 font-semibold">
+          Hoping to find a partner in crime in building the next big thing.      
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss">
+  .avatar {
+    @apply block mx-auto h-48 rounded-full sm:mx-0 sm:flex-shrink-0;
+  }
+
+  .hero-text {
+    @apply text-xl leading-7 font-semibold flex justify-center py-2;
+  }
+</style>
